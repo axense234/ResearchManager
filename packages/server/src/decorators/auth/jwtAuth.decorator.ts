e7f3387d05667dcc6cd92c398e3225d62f04048a
axense234 <andreicomanescuonline@gmail.com>
2025-05-22 23:09:08 +0300

    Possible bug fix #3.

--- packages/server/src/decorators/auth/jwtAuth.decorator.ts
+++ packages/server/src/decorators/auth/jwtAuth.decorator.ts
@@ -9,12 +9,12 @@ export const JwtAuth = (options?: {
 }) => {
   if (options?.alwaysSkipAuthentication) {
     return applyDecorators();
+  } else {
+    const shouldSkipAuthentication =
+      process.env.NODE_ENV === 'development' && !options?.profileRoute;
+
+    return shouldSkipAuthentication
+      ? applyDecorators()
+      : applyDecorators(UseGuards(JwtGuard));
   }
-
-  const shouldSkipAuthentication =
-    process.env.NODE_ENV === 'development' && !options?.profileRoute;
-
-  return shouldSkipAuthentication
-    ? applyDecorators()
-    : applyDecorators(UseGuards(JwtGuard));
 };
